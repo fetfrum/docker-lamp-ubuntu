4ac93338ad3536cbae66a3cdb5446c62c13387d3
Igor V. Kruglenko <igor.kruglenko@gmail.com>
2017-11-01 15:04:26 +0200

    packages.sh

--- root/packages.sh
+++ root/packages.sh
@@ -1,4 +1,27 @@
 #/bin/bash
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install supervisor \
-mariadb-server mariadb-client install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache  php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext php7.0-opcache php-apcu phpmyadmin
+mariadb-server \
+mariadb-client \
+php7.0 \
+libapache2-mod-php7.0 \
+php7.0-mysql \
+php7.0-curl \
+php7.0-gd \
+php7.0-intl \
+php-pear \
+php-imagick \
+php7.0-imap \
+php7.0-mcrypt \
+php-memcache \
+php7.0-pspell \
+php7.0-recode \
+php7.0-sqlite3 \
+php7.0-tidy \
+php7.0-xmlrpc \
+php7.0-xsl \
+php7.0-mbstring \
+php-gettext \
+php7.0-opcache \
+php-apcu \
+phpmyadmin
